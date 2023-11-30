b2976e6f21b911d94a5c9f657789cb5eaa68e1e0
qkrrkddud <ejfehd1@naver.com>
2023-11-30 17:07:29 +0900

    2차수정-박강영

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-<<<<<<< HEAD
 import SearchMain from '../views/SearchMain.vue'
-=======
-import HomeView from '../views/HomeView.vue'
 import HeaderLogin from '../components/HeaderLogin.vue'
->>>>>>> d9f79f361c14827eb19c880e6521a455be7026f3
 
 const routes = [
   {
@@ -12,23 +8,12 @@ const routes = [
     name: 'search',
     component: SearchMain
   },
-<<<<<<< HEAD
   
-=======
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
   {
     path: '/login',
     name: 'login',
     component: HeaderLogin
   },
->>>>>>> d9f79f361c14827eb19c880e6521a455be7026f3
 ]
 
 const router = createRouter({
