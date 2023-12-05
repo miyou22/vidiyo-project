729f847d70ae6a956765c1689f139836836c16c6
macaron13 <eequalhv@gmail.com>
2023-12-05 18:20:58 +0900

    jiyoon1205-3

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-    <h1>길 징거</h1>
+  <div class="wrap">
+    <h1>길 정거</h1>
+    <div class="content">
+      <div class="row g-0">
+        <div class="col" v-for="image in images" :key="image">
+          <img :src="image.img" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      images:[
+        {
+          img: require("../assets/image/poster1.jpg"),
+        },
+        {
+          img: require("../assets/image/poster2.jpg"),
+        },
+        {
+          img: require("../assets/image/poster3.jpg"),
+        },
+        {
+          img: require("../assets/image/poster4.jpg"),
+        },
+        ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .wrap{
+    color: #fff;
+  }
+  h1{
+    font-size: 28px;
+    font-weight: bold;
+    margin-bottom: 2.25rem;
+  }
+  .col >img{
+    width: 190px;
+    height: 280px;
+    border-radius: 5px;
+  }
 </style>
