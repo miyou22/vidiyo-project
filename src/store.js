a253c64e9d9d2a39d6adc7e172408024cb054dc6
seobi <miyou22@naver.com>
2023-12-05 18:41:17 +0900

    seobi1205-1

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state() { //프로젝트 전체에서 공통으로 사용할 변수를 정의하는 곳
+    return {
+    }
+  },
+	//변수는 반드시 mutations을 이용해서 변경해야 합니다.(동기처리:Sync)
+  mutations: {
+   
+  }
+})
+
+export default store;
