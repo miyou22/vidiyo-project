35c4cf2540e470c77057a742c4afb27397ebfbd8
seobi <miyou22@naver.com>
2023-12-06 10:14:50 +0900

    1206-2

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -5,10 +5,10 @@
     </a>
     <div class="menu">
       <ul class="gnb">
-        <li><a href="#"><img src="../assets/image/home.png" alt="">홈</a></li>
-        <li><a href="#"><img src="../assets/image/star_border.png" alt="">평가</a></li>
-        <li><a href="#"><img src="../assets/image/search.png" alt="">검색</a></li>
-        <li><a href="#"><img src="../assets/image/box.png" alt="">보관함</a></li>
+        <li class="svg1"><a href="#"><img src="../assets/image/home.png" alt=""><span class="menu">홈</span></a></li>
+        <li class="svg2"><a href="#"><img src="../assets/image/star_border.png" alt=""><span class="menu">평가</span></a></li>
+        <li class="svg3"><a href="#"><img src="../assets/image/search.png" alt=""><span class="menu">검색</span></a></li>
+        <li class="svg4"><a href="#"><img src="../assets/image/box.png" alt=""><span class="menu">보관함</span></a></li>
       </ul>
     </div>
   </div>
@@ -30,19 +30,20 @@ export default {
     left: 0;
     top: 0;
     box-sizing: border-box;
-    /* z-index: 501; */
+    z-index: 5010;
   }
   .icon{
     display: block;
-    margin: 24px 0 0 26px;
+    margin: 24px 0 0 16px;
   }
   .icon img{
-
+    
   }
   .gnb{
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    justify-content: center;  
     padding: 0 1rem;
     margin-top: 3.375rem;
   }
@@ -53,7 +54,27 @@ export default {
   }
   li > a >img{
     margin-right: 1rem;
-
+  }
+  .svg1{
+    margin-left: -2px;
+  }
+  .svg2{
+    margin-left: -4px;
+  }
+  .svg3{
+    margin-left: -2px;
+  }
+  .svg1 .menu{
+    margin-left: 10px;
+  }
+  .svg2 .menu{
+    margin-left: 8px;
+  }
+  .svg3 .menu{
+    margin-left: 9px;
+  }
+  .svg4 .menu{
+    margin-left: 14px;
   }
 
 
